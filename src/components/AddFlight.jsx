53ec20f1e58fa22b46ba1261ca09a09b92d4f4b6
sandrajoseph111 <sandrajosephkodiyattu@gmail.com>
2026-07-13 13:42:41 +0530

    Navigation added

--- src/components/AddFlight.jsx
+++ src/components/AddFlight.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import NavigationBar from './NavigationBar'
 
 const AddFlight = () => {
 
@@ -55,7 +56,7 @@ const AddFlight = () => {
     }
   return (
     <div>
-
+        <NavigationBar />
         <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
